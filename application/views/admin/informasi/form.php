9b1e5cdfdbe4b07d5ed9793b98fa8c3baef57c54
ErickWahyudy <erickwahyudy@gmail.com>
2023-03-12 21:10:32 +0700

    migrasi to api & ajax

--- application/views/admin/informasi/form.php
+++ application/views/admin/informasi/form.php
@@ -21,7 +21,7 @@
                 <td><?= $no ?></td>
                 <td><?= $informasi['informasi'] ?></td>
                 <td>
-                    <a href="<?= base_url('template/file_informasi/'.$informasi['berkas']) ?>"
+                    <a href="<?= base_url('themes/file_informasi/'.$informasi['berkas']) ?>"
                         target="_blank"><?= $informasi['berkas'] ?></a> <br>
                     <a href="" class="btn btn-info" data-toggle="modal"
                         data-target="#editFile<?= $informasi['id_informasi'] ?>"><i class="fa fa-upload"></i></a>
@@ -48,8 +48,7 @@
                 </div>
                 <div class="modal-body table-responsive">
                     <table class="table table-bordered table-striped">
-                        <form action="<?= base_url('admin/informasi/add') ?>" method="post"
-                            enctype="multipart/form-data">
+                        <form id="add">
                             <tr>
                                 <th>Jenis Informasi</th>
                             </tr>
@@ -63,7 +62,7 @@
                                 <td>
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Kembali</button>
                                     &nbsp;&nbsp;
-                                    <input type="submit" name="kirim" value="Submit" class="btn btn-success">
+                                    <button type="submit" name="kirim" class="btn btn-success">Simpan</button>
                                 </td>
                             </tr>
 
@@ -88,8 +87,7 @@
                 </div>
                 <div class="modal-body table-responsive">
                     <table class="table table-bordered table-striped">
-                        <form action="<?= base_url('admin/informasi/edit/'.$informasi['id_informasi']) ?>" method="post"
-                            enctype="multipart/form-data">
+                        <form id="edit" method="post">
                             <tr>
                                 <th class="col-md-2">ID Informasi</th>
                             </tr>
@@ -113,11 +111,11 @@
                                 <td>
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Kembali</button>
                                     &nbsp;&nbsp;
-                                    <input type="submit" name="kirim" value="Simpan" class="btn btn-success">
+                                    <button type="submit" name="kirim" class="btn btn-success">Simpan</button>
                                     &nbsp;&nbsp;
-                                    <a href="<?= base_url('admin/informasi/hapus/'.$informasi['id_informasi']) ?>"
-                                        class="btn btn-danger" onclick="return confirm('Yakin Hapus Data Ini ?')"><i
-                                            class="fa fa-trash"></i> Hapus</a>
+                                    <!-- memanggil javascript hapus -->
+                                    <a href="javascript:void(0)" onclick="hapusInformasi('<?= $informasi['id_informasi'] ?>')"
+                                        class="btn btn-danger">Hapus</a>
                                 </td>
                             </tr>
 
@@ -127,8 +125,8 @@
             </div>
         </div>
     </div>
-    <?php endforeach; ?>
     <!-- End Modal -->
+    <?php endforeach; ?>    
 
     <!-- Modal edit File informasi-->
     <?php foreach($data->result_array() as $informasi): ?>
@@ -143,26 +141,23 @@
                 </div>
                 <div class="modal-body table-responsive">
                     <table class="table table-bordered table-striped">
-                        <form action="<?= base_url('admin/informasi/file/'.$informasi['id_informasi']) ?>" method="post"
-                            enctype="multipart/form-data">
+                    <form id="uploadFile" method="post">
                             <tr>
                                 <th class="col-md-2">ID Informasi</th>
                             </tr>
                             <tr>
                                 <td>
-                                    <p class="form-control"><?= $informasi['id_informasi'] ?> |
-                                        <?= $informasi['informasi'] ?></p>
+                                    <input type="text" name="id_informasi" value="<?= $informasi['id_informasi'] ?>"
+                                        class="form-control" readonly>
                                 </td>
                             </tr>
                             <tr>
-                                <th>Upload file</th>
+                                <th>File</th>
                             </tr>
                             <tr>
                                 <td>
-                                    <input type="text" name="berkas" value="<?= $informasi['berkas'] ?>"
-                                        class="form-control" readonly>
                                     <input type="file" name="berkas" value="<?= $informasi['berkas'] ?>"
-                                        class="form-control" required="">
+                                        class="form-control">
                                 </td>
                             </tr>
 
@@ -170,11 +165,13 @@
                                 <td>
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Kembali</button>
                                     &nbsp;&nbsp;
-                                    <input type="submit" name="kirim" value="Simpan" class="btn btn-success">
+                                    <button type="submit" name="kirim" class="btn btn-success">Simpan</button>
+                                    
                                 </td>
                             </tr>
 
                         </form>
+
                     </table>
                 </div>
             </div>
@@ -182,6 +179,165 @@
     </div>
     <?php endforeach; ?>
     <!-- End Modal -->
+
+<script>
+        //add data
+        $(document).ready(function () {
+        $('#add').submit(function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: "<?= site_url('admin/informasi/api_add') ?>",
+                type: "POST",
+                data: new FormData(this),
+                processData: false,
+                contentType: false,
+                cache: false,
+                async: false,
+                success: function (data) {
+                    $('#modalTambahInformasi');
+                    $('#add')[0].reset();
+                    swal({
+                        title: "Berhasil",
+                        text: "Data berhasil ditambahkan",
+                        type: "success",
+                        showConfirmButton: true,
+                        confirmButtonText: "OKEE",
+                    }).then(function () {
+                        location.reload();
+                    });
+                }
+            });
+        });
+    });
+
+        //edit file
+        $(document).on('submit', '#edit', function(e) {
+        e.preventDefault();
+        var form_data = new FormData(this);
+
+        $.ajax({
+            type: "POST",
+            url: "<?php echo site_url('admin/informasi/api_edit/') ?>" + form_data.get('id_informasi'),
+            dataType: "json",
+            data: form_data,
+            processData: false,
+            contentType: false,
+            //memanggil swall ketika berhasil
+            success: function(data) {
+                $('#edit' + form_data.get('id_informasi'));
+                swal({
+                    title: "Berhasil",
+                    text: "Data Berhasil Diubah",
+                    type: "success",
+                    showConfirmButton: true,
+                    confirmButtonText: "OKEE",
+                }).then(function() {
+                    location.reload();
+                });
+            },
+            //memanggil swall ketika gagal
+            error: function(data) {
+                swal({
+                    title: "Gagal",
+                    text: "Data Gagal Diubah",
+                    type: "error",
+                    showConfirmButton: true,
+                    confirmButtonText: "OKEE",
+                }).then(function() {
+                    location.reload();
+                });
+            }
+        });
+    });
+
+        //ajax upload file ke database dan folder
+        $(document).on('submit', '#uploadFile', function(e) {
+        e.preventDefault();
+        var form_data = new FormData(this);
+
+        $.ajax({
+            type: "POST",
+            url: "<?php echo site_url('admin/informasi/api_file/') ?>" + form_data.get('id_informasi'),
+            dataType: "json",
+            data: form_data,
+            processData: false,
+            contentType: false,
+            //memanggil swall ketika berhasil
+            success: function(data) {
+                $('#uploadFile' + form_data.get('id_informasi'));
+                swal({
+                    title: "Berhasil",
+                    text: "Berhasil upload file",
+                    type: "success",
+                    showConfirmButton: true,
+                    confirmButtonText: "OKEE",
+                }).then(function() {
+                    location.reload();
+                });
+            },
+            //memanggil swall ketika gagal
+            error: function(data) {
+                swal({
+                    title: "Gagal",
+                    text: "Gagal upload file",
+                    type: "error",
+                    showConfirmButton: true,
+                    confirmButtonText: "OKEE",
+                }).then(function() {
+                    location.reload();
+                });
+            }
+        });
+    });
+
+
+        //ajax hapus informasi
+        function hapusInformasi(id_informasi) {
+        swal({
+            title: "Apakah Anda Yakin?",
+            text: "Data Akan Dihapus",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Ya, Hapus!",
+            cancelButtonText: "Tidak, Batalkan!",
+            closeOnConfirm: false,
+            closeOnCancel: true // Set this to true to close the dialog when the cancel button is clicked
+        }).then(function(result) {
+            if (result.value) { // Only delete the data if the user clicked on the confirm button
+                $.ajax({
+                    type: "POST",
+                    url: "<?php echo site_url('admin/informasi/api_hapus/') ?>" + id_informasi,
+                    dataType: "json",
+                }).done(function() {
+                    swal({
+                        title: "Berhasil",
+                        text: "Data Berhasil Dihapus",
+                        type: "success",
+                        showConfirmButton: true,
+                        confirmButtonText: "OKEE"
+                    }).then(function() {
+                        location.reload();
+                    });
+                }).fail(function() {
+                    swal({
+                        title: "Gagal",
+                        text: "Data Gagal Dihapus",
+                        type: "error",
+                        showConfirmButton: true,
+                        confirmButtonText: "OKEE"
+                    }).then(function() {
+                        location.reload();
+                    });
+                });
+            } else { // If the user clicked on the cancel button, show a message indicating that the deletion was cancelled
+                swal("Batal hapus", "Data Tidak Jadi Dihapus", "error");
+            }
+        });
+    }
+    </script>
+    
+    
 
 <?php $this->load->view('template/footer'); ?>
 
